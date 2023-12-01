63bc523043d42e4270ba6a636b0a9725eb4ddfbb
kei <lab.sprout@gmail.com>
2023-12-01 18:05:05 +0900

    add: main.pyを作成

---
+++ main.py
@@ -0,0 +1,105 @@
+import time
+
+# ユーザーリスト
+user_list = []
+
+# メッセージを保存する辞書
+message_dict = {}
+
+# メッセージを表す構造体
+class Message:
+    from_id = ""
+    to_id = ""
+    content = ""
+    time = 0
+
+    def __init__(self, from_id, to_id, content, time):
+        self.from_id = from_id
+        self.to_id = to_id
+        self.content = content
+        self.time = time
+    
+    # メッセージの整形
+    def getMessage(self):
+        return "{} -> {}: {}".format(self.from_id, self.to_id, self.content)
+
+# ユーザーを追加する
+def add_user(user_id):
+    # ユーザーIDが既に登録されているかチェック
+    if user_id in user_list:
+        return "ERROR: ID already used!"
+    else:
+        # 登録されていなかったらユーザーリストに追加
+        user_list.append(user_id)
+        # メッセージを保存する辞書に追加
+        message_dict[user_id] = {}
+        return "{} registered!".format(user_id)
+
+# ユーザーにメッセージを送る
+def talk(from_id, to_id, content):
+    # ユーザーがユーザーリストに存在するかチェック
+    if (from_id not in user_list) or (to_id not in user_list):
+        return "ERROR: no user ID"
+    else:
+        # メッセージを作成
+        message = Message(from_id, to_id, content, time.time())
+
+        # メッセージの保存
+        if to_id not in message_dict[from_id]:
+            message_dict[from_id][to_id] = [message]
+        else:
+            message_dict[from_id][to_id].append(message)
+
+        return ""
+
+# ログを表示する
+def show_log(id_1, id_2):
+    # ユーザーがユーザーリストに存在するかチェック
+    if (id_1 not in user_list) or (id_2 not in user_list):
+        return "ERROR: no user ID"
+    else:
+        # メッセージの一覧を取得
+        msg_list1 = message_dict[id_1][id_2]
+        msg_list2 = message_dict[id_2][id_1]
+        msg_list_all = msg_list1 + msg_list2
+
+        # メッセージの時刻でソート
+        msg_list_all.sort(key=lambda x: x.time)
+
+        # メッセージの取得
+        res = ""
+        for message in msg_list_all:
+            res += message.getMessage() + "\n"
+
+        return res.rstrip()
+
+# メイン関数
+def main():
+    while True:
+        try:
+            # 標準入力を受け取り空白で分割する
+            cmd = input("input: ").split(' ', 3)
+            res = "" # 出力する文字列
+
+            # add_user コマンド
+            if cmd[0] == "add_user" and len(cmd) == 2:
+                res = add_user(cmd[1])
+
+            # talk コマンド
+            elif cmd[0] == "talk" and len(cmd) == 4:
+                res = talk(cmd[1], cmd[2], cmd[3])
+
+            # show_log コマンド
+            elif cmd[0] == "show_log" and len(cmd) == 3:
+                res = show_log(cmd[1], cmd[2])
+
+            # 標準出力に返答
+            if res != "":
+                print("OUTPUT: {}".format(res))
+
+        except EOFError:
+            # Ctrl + D で終了
+            break
+
+if __name__ == "__main__":
+    main()
