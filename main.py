461638d742bdf2d0d0a36ff31801bacebd215523
kei <lab.sprout@gmail.com>
2023-12-06 19:52:21 +0900

    fix: コードレビューをもとに修正（まとめてやっちゃった）

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 import time
-
-# ユーザーリスト
-user_list = []
-
-# メッセージを保存する辞書
-message_dict = {}
+from typing import List, Dict
 
 # メッセージを表す構造体
 class Message:
-    from_id = ""
-    to_id = ""
-    content = ""
-    time = 0
-
     def __init__(self, from_id, to_id, content, time):
         self.from_id = from_id
         self.to_id = to_id
         self.content = content
         self.time = time
-    
+        
     # メッセージの整形
     def getMessage(self):
         return "{} -> {}: {}".format(self.from_id, self.to_id, self.content)
+
+# ユーザーリスト
+user_list: List[str] = []
+
+# メッセージを保存する辞書
+message_dict: Dict[str, Dict[str, List[Message]]]= {}
 
 # ユーザーを追加する
 def add_user(user_id):
@@ -40,6 +36,8 @@ def talk(from_id, to_id, content):
     # ユーザーがユーザーリストに存在するかチェック
     if (from_id not in user_list) or (to_id not in user_list):
         return "ERROR: no user ID"
+    elif from_id == to_id:
+        return "Error: same user refered"
     else:
         # メッセージを作成
         message = Message(from_id, to_id, content, time.time())
@@ -57,6 +55,8 @@ def show_log(id_1, id_2):
     # ユーザーがユーザーリストに存在するかチェック
     if (id_1 not in user_list) or (id_2 not in user_list):
         return "ERROR: no user ID"
+    elif id_1 == id_2:
+        return "Error: same user refered"
     else:
         # メッセージの一覧を取得
         msg_list1 = message_dict[id_1][id_2]
@@ -67,18 +67,14 @@ def show_log(id_1, id_2):
         msg_list_all.sort(key=lambda x: x.time)
 
         # メッセージの取得
-        res = ""
-        for message in msg_list_all:
-            res += message.getMessage() + "\n"
-
-        return res.rstrip()
+        return "\n".join([msg.getMessage() for msg in msg_list_all])
 
 # メイン関数
 def main():
     while True:
         try:
             # 標準入力を受け取り空白で分割する
-            cmd = input("input: ").split(' ', 3)
+            cmd = input("input: ").split(' ', 3) # talkコマンドでメッセージに空白が含まれるため3に設定
             res = "" # 出力する文字列
 
             # add_user コマンド
